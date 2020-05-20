14b1da8d454b23f0d2834a379bab2aecc4cd98c8
danneg <dana@boxalino.com>
2020-05-20 13:48:43 +0200

    added CMS context

--- src/Framework/Request/Context/Cms.php
+++ src/Framework/Request/Context/Cms.php
@@ -1,41 +1,61 @@
 <?php declare(strict_types=1);
 namespace Boxalino\RealTimeUserExperienceIntegration\Framework\Request\Context;
 
-use Boxalino\RealTimeUserExperience\Framework\Request\CmsContextAbstract;
+use Boxalino\RealTimeUserExperience\Framework\Request\ContextAbstract;
+use Boxalino\RealTimeUserExperience\Framework\Request\ListingContextAbstract;
 use Shopware\Core\Content\Product\Aggregate\ProductVisibility\ProductVisibilityDefinition;
+use Shopware\Core\System\SalesChannel\SalesChannelContext;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
- * CMS request
+ * Navigation request
+ *
+ * A listing context can render a default Category/Search view layout:
+ * facets, products, sorting, pagination and other narrative elements
  *
  * @package Boxalino\RealTimeUserExperienceIntegration\Framework\Request\Context
  */
-class Cms extends CmsContextAbstract
+class Cms extends ListingContextAbstract
 {
-
-    /**
-     * If there are configured returnFields in the CMS element - they will be used
-     *
-     * @return array
-     */
-    public function getReturnFields() : array
-    {
-        return ["id", "products_group_id"];
-    }
-
     /**
      * @return int
      */
     public function getContextVisibility() : int
     {
-        return ProductVisibilityDefinition::VISIBILITY_SEARCH;
+        return ProductVisibilityDefinition::VISIBILITY_ALL;
     }
 
     /**
-     * Range properties
+     * @param Request $request
+     * @return string
+     */
+    public function getContextNavigationId(Request $request): array
+    {
+        $params = $request->attributes->get('_route_params');
+        if ($params && isset($params['navigationId']))
+        {
+            return [$params['navigationId']];
+        }
+
+        return [$this->getSalesChannelContext()->getSalesChannel()->getNavigationCategoryId()];
+    }
+
+    /**
+     * Set the return fields desired for navigation
      *
      * @return array
      */
-    public function getRangeProperties(): array
+    public function getReturnFields() : array
+    {
+        return ["id", "products_group_id","discountedPrice", "products_seo_url", "title", "products_image"];
+    }
+
+    /**
+     * Set the range properties following the presented structure
+     *
+     * @return array
+     */
+    public function getRangeProperties() : array
     {
         return [
             "products_rating_average" => ['from' => 'products_rating_average', 'to' => 0],
