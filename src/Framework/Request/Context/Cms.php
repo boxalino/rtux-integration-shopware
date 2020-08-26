484ba854c125120427d7c3569832a1102c8930a2
danneg <dana@boxalino.com>
2020-08-27 01:12:27 +0200

    [cms] inheritance update

--- src/Framework/Request/Context/Cms.php
+++ src/Framework/Request/Context/Cms.php
@@ -1,46 +1,28 @@
 <?php declare(strict_types=1);
 namespace Boxalino\RealTimeUserExperienceIntegration\Framework\Request\Context;
 
-use Boxalino\RealTimeUserExperience\Framework\Request\ContextAbstract;
-use Boxalino\RealTimeUserExperience\Framework\Request\ListingContextAbstract;
-use Boxalino\RealTimeUserExperienceApi\Service\Api\Request\RequestInterface;
+use Boxalino\RealTimeUserExperience\Framework\Request\CmsContextAbstract;
 use Boxalino\RealTimeUserExperienceIntegration\Framework\Request\IntegrationContextTrait;
 use Shopware\Core\Content\Product\Aggregate\ProductVisibility\ProductVisibilityDefinition;
-use Shopware\Core\System\SalesChannel\SalesChannelContext;
 
 /**
- * Navigation request
+ * CMS request
  *
- * A listing context can render a default Category/Search view layout:
- * facets, products, sorting, pagination and other narrative elements
+ * A CMS context can render a default Category view layout:
+ * facets, products, sorting, pagination and other narrative CMS elements
  *
  * @package Boxalino\RealTimeUserExperienceIntegration\Framework\Request\Context
  */
-class Cms extends ListingContextAbstract
+class Cms extends CmsContextAbstract
 {
     use IntegrationContextTrait;
-    
+
     /**
      * @return int
      */
     public function getContextVisibility() : array
     {
         return [ProductVisibilityDefinition::VISIBILITY_ALL];
-    }
-
-    /**
-     * @param RequestInterface $request
-     * @return string
-     */
-    public function getContextNavigationId(RequestInterface $request): array
-    {
-        $params = $request->getRequest()->attributes->get('_route_params');
-        if ($params && isset($params['navigationId']))
-        {
-            return [$params['navigationId']];
-        }
-
-        return [$this->getSalesChannelContext()->getSalesChannel()->getNavigationCategoryId()];
     }
 
     /**
