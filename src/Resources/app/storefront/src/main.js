e354b6a63f76425cf7b7bb3820afc4bb2961f6a8
Marcel Joss <marcel.joss@perfecthair.ch>
2020-10-02 14:23:14 +0200

    Fix filter removal by using the right field names.

--- src/Resources/app/storefront/src/main.js
+++ src/Resources/app/storefront/src/main.js
@@ -1,7 +1,9 @@
 import RtuxListingFilterPlugin from './rtux-listing-filter/rtux-listing-filter.plugin';
 import RtuxAutocompletePlugin from './rtux-autocomplete/rtux-autocomplete.plugin';
+import RtuxFilterRatingPlugin from './rtux-listing-filter/rtux-filter-rating.plugin';
 
 const PluginManager = window.PluginManager;
 
 PluginManager.register('RtuxListingFilterPlugin', RtuxListingFilterPlugin, '[data-rtux-listing-filter]');
 PluginManager.extend('SearchWidget', 'SearchWidget', RtuxAutocompletePlugin, '[data-search-form]');
+PluginManager.override('FilterRating', RtuxFilterRatingPlugin, '[data-filter-rating]')
