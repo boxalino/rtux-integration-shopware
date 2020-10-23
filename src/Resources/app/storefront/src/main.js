43f238d4c37b70d5c7be3ec0280488122e5746a0
danneg <dana@boxalino.com>
2020-10-23 20:48:43 +0200

    [init]

--- src/Resources/app/storefront/src/main.js
+++
@@ -1,9 +0,0 @@
-import RtuxListingFilterPlugin from './rtux-listing-filter/rtux-listing-filter.plugin';
-import RtuxAutocompletePlugin from './rtux-autocomplete/rtux-autocomplete.plugin';
-import RtuxFilterRatingPlugin from './rtux-listing-filter/rtux-filter-rating.plugin';
-
-const PluginManager = window.PluginManager;
-
-PluginManager.register('RtuxListingFilterPlugin', RtuxListingFilterPlugin, '[data-rtux-listing-filter]');
-PluginManager.extend('SearchWidget', 'SearchWidget', RtuxAutocompletePlugin, '[data-search-form]');
-PluginManager.override('FilterRating', RtuxFilterRatingPlugin, '[data-filter-rating]')
