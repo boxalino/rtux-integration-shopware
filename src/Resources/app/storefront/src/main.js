2ead34cc570f20c25b6b7acc732a64969c0bf744
danneg <dana@boxalino.com>
2021-01-19 15:28:09 +0100

    [facet][js] update for multi-select facet options to allow same-value options select

--- src/Resources/app/storefront/src/main.js
+++ src/Resources/app/storefront/src/main.js
@@ -1,9 +1,11 @@
 import RtuxListingFilterPlugin from './rtux-listing-filter/rtux-listing-filter.plugin';
 import RtuxAutocompletePlugin from './rtux-autocomplete/rtux-autocomplete.plugin';
 import RtuxFilterRatingPlugin from './rtux-listing-filter/rtux-filter-rating.plugin';
+import RtuxFilterMultiSelectPlugin from './rtux-listing-filter/rtux-filter-multi-select.plugin';
 
 const PluginManager = window.PluginManager;
 
 PluginManager.register('RtuxListingFilterPlugin', RtuxListingFilterPlugin, '[data-rtux-listing-filter]');
 PluginManager.extend('SearchWidget', 'SearchWidget', RtuxAutocompletePlugin, '[data-search-form]');
-PluginManager.override('FilterRating', RtuxFilterRatingPlugin, '[data-filter-rating]')
+PluginManager.register('RtuxFilterRating', RtuxFilterRatingPlugin, '[data-rtux-filter-rating]')
+PluginManager.register('RtuxFilterMultiSelect', RtuxFilterMultiSelectPlugin, '[data-rtux-filter-multi-select]')
---
+++ src/Resources/app/storefront/src/rtux-listing-filter/rtux-filter-multi-select.plugin.js
@@ -0,0 +1,92 @@
+import DomAccess from 'src/helper/dom-access.helper';
+import Iterator from 'src/helper/iterator.helper';
+import FilterMultiSelectPlugin from 'src/plugin/listing/filter-multi-select.plugin';
+import deepmerge from 'deepmerge';
+
+/**
+ * Extended from the default filter-multer-select plugin
+ * In order to use "value" for the URL (SEO)
+ * and to use different id-value on the checkboxes (to allow same-value facet options across facets)
+ */
+export default class RtuxFilterMultiSelectPlugin extends FilterMultiSelectPlugin {
+
+    static options = deepmerge(FilterMultiSelectPlugin.options, {
+    });
+
+    getValues() {
+        const checkedCheckboxes =
+            DomAccess.querySelectorAll(this.el, `${this.options.checkboxSelector}:checked`, false);
+
+        let selection = [];
+
+        if (checkedCheckboxes) {
+            Iterator.iterate(checkedCheckboxes, (checkbox) => {
+                selection.push(checkbox.value);
+            });
+        } else {
+            selection = [];
+        }
+
+        this.selection = selection;
+        this._updateCount();
+
+        const values = {};
+        values[this.options.name] = selection;
+
+        return values;
+    }
+    
+    /**
+     *
+     * @param params
+     * @returns {boolean}
+     */
+    setValuesFromUrl(params) {
+        let stateChanged = false;
+        Object.keys(params).forEach(key => {
+            if (key === this.options.name) {
+                stateChanged = true;
+                const ids = params[key].split('|');
+
+                ids.forEach(id => {
+                    const checkboxEl = DomAccess.querySelector(this.el, `[value="${id}"]`, false);
+
+                    if (checkboxEl) {
+                        checkboxEl.checked = true;
+                        this.selection.push(checkboxEl.value);
+                    }
+                });
+            }
+        });
+
+        super._updateCount();
+
+        return stateChanged;
+    }
+
+
+    /**
+     * @return {Array}
+     * @public
+     */
+    getLabels() {
+        const activeCheckboxes =
+            DomAccess.querySelectorAll(this.el, `${this.options.checkboxSelector}:checked`, false);
+
+        let labels = [];
+
+        if (activeCheckboxes) {
+            Iterator.iterate(activeCheckboxes, (checkbox) => {
+                labels.push({
+                    label: checkbox.dataset.label,
+                    value: checkbox.dataset.value,
+                    id: checkbox.id
+                });
+            });
+        } else {
+            labels = [];
+        }
+
+        return labels;
+    }
+}
